6fcda7919bd64175150c237647c66fe75a2f8aed
Siwei Shen <sg552sg552@gmail.com>
2012-05-17 10:28:50 +0800

    removed scaffold: crawlings

--- app/models/crawling.rb
+++
@@ -1,6 +0,0 @@
-class Crawling
-  include Mongoid::Document
-  field :url, :type => String
-  field :key_word, :type => String
-  field :result, :type => String
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 MiaomiaoCat::Application.routes.draw do
   resources :source_websites
   resources :items
-  root :to => 'items#index'
-  resources :crawlings
-end
+  root :to => 'items#index'end
