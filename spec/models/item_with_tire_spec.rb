decf88f66aaed9b6563249ebcd07e8f85910cb29
Siwei Shen <sg552sg552@gmail.com>
2012-06-21 13:44:37 +0800

    added spec file for: tire

---
+++ spec/models/item_with_tire_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+#Tire.index 'articles' do
+#  delete
+#  create
+#  store :title => "one", :tags => ['ruby']
+#  store :title => "two", :tags => ['ruby', 'python']
+#  store :title => "three", :tags => ['java']
+#  store :title => "four", :tags => ['c++']
+#  refresh
+#end
+    #articles = [
+    #  { :id => '1', :type => 'article', :title => 'one',   :tags => ['ruby']           },
+    #  { :id => '2', :type => 'article', :title => 'two',   :tags => ['ruby', 'python'] },
+    #  { :id => '3', :type => 'article', :title => 'three', :tags => ['java']           },
+    #  { :id => '4', :type => 'article', :title => 'four',  :tags => ['ruby', 'php']    }
+    #]
+
+    #Tire.index 'articles' do
+    #  import articles do |documents|
+    #    documents.each { |document| document[:title].capitalize! }
+    #  end
+    #  refresh
+    #end
+
+describe Item do
+  it "should create" do
+    index = Tire::Index.new "wokao"
+    index.delete
+    index.create
+    index.store :title => "wo le ge qu~"
+    index.refresh
+  end
+  it "should query" do
+    s = Tire.search 'articles' do
+      query do
+        string 'title:*o*'
+      end
+      filter :term, :tags => ['ruby']
+      sort { by :title , 'desc'}
+      facet 'global-tags', :global => true do
+        terms :tags
+      end
+      facet 'current-tags' do
+        terms :tags
+      end
+    end
+    s.results.each do |document|
+      puts "* #{document.title} [tags: #{document.tags.join(",")}]"
+    end
+    s.results.facets['global-tags']['terms'].each do |f|
+      puts "#{f['term'].ljust(10)} #{f['count']}"
+    end
+    s.results.facets['current-tags']['terms'].each do |f|
+      puts "#{f['term'].ljust(10)} #{f['count']}"
+    end
+  end
+end
