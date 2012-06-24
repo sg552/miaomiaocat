d08c1dc51f1d304eadab60b5fd7292563cf17840
Shen Siwei <sg552sg552@gmail.com>
2012-06-24 14:01:59 +0800

    used tire for search

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,13 @@ class ItemsController < ApplicationController
   # GET /items.json
   def index
     @key_word = params[:key_word]
-    @items = Item.where(:content =>/#{@key_word}/).desc(:created_at).page params[:page]
+    #@items = Item.where(:content =>/#{@key_word}/).desc(:created_at).page params[:page]
+    if params[:key_word].blank?
+      @items = Item.desc(:_id).page params[:page]
+    else
+      #@items = Item.tire.search(params[:key_word]).page(params[:page])
+      @items = Item.tire.search(params[:key_word], :page => (params[:page] || 1))
+    end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @items }
